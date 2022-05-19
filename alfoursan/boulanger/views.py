063daf45b48e2275cc511692bdadbf638a3c5f24
zepekenhio <moukhtar.benali@gmail.com>
2022-05-19 17:34:06 +0200

    Deleted the file from the git repository

--- alfoursan/boulanger/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-#@login_required
-def accueil(request):
-    return render(request, 'boulanger/accueil.html', {})
